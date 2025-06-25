60ef2550d8756a05a89fb43b8460d734e1575acb
Kaio Garcia - Desenvolvimento - Employer <kaiogarcia@employer.com.br>
2025-06-25 16:14:42 -0300

    Change program cs

--- RabbitMQDemo.ConsumerAPI/Program.cs
+++ RabbitMQDemo.ConsumerAPI/Program.cs
@@ -2,21 +2,19 @@ using RabbitMQDemo.Application.Interfaces;
 using RabbitMQDemo.Application.Services;
 using RabbitMQDemo.Domain.Interfaces;
 using RabbitMQDemo.Domain.Services.Implementations;
-using RabbitMQDemo.Domain.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IRabbitMQConsumerApplicationService, RabbitMQConsumerApplicationService>();
-builder.Services.AddScoped<IRabbitMQConsumerService, RabbitMQConsumerService>();
-builder.Services.AddScoped<IMessageHandler, MessageHandler>();
+builder.Services.AddScoped<IRabbitMQSendApplicationService, RabbitMQSendApplicationService>();
+builder.Services.AddScoped<IRabbitMQSenderService, RabbitMQSenderService>();
 
 
 var app = builder.Build();
+
 
 if (app.Environment.IsDevelopment())
 {
